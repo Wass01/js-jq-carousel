c0b466c2156b23590a98ccadff32f3d1ccbf2f2b
wass01 <maatallawassim01@gmail.com>
2021-03-11 15:34:43 +0100

    variabili e funzione

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function (){
+  slideImg();
+});
+
+function slideImg() {
+  var slideDx = $(".next > i");
+  var slideSx = $(".prev > i");
+  var image = $(".images > img");
+
+  slideDx.click(function (){
+    image.removeClass("active").next().addClass("active");
+
+
+  });
+}
