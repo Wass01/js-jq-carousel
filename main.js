eb93a957e8d06abe556bba5912a7fd6efb107aa0
wass01 <maatallawassim01@gmail.com>
2021-03-11 15:59:23 +0100

    cirlcle cambia posizione

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ function slideImg() {
   var slideDx = $(".next > i");
   var slideSx = $(".prev > i");
   var image = $(".images > .first");
+  var circle = $(".nav > .first");
 
   slideDx.click(function (){
     image.removeClass("active").next().addClass("active");
-    
+
+
+    circle.removeClass("active").next().addClass("active");
+
   });
 }
