8052d732abbd24de34d13e970f9bf7fe0cfc43a5
wass01 <maatallawassim01@gmail.com>
2021-03-12 10:40:28 +0100

    ESERCIZIO TERMINATO
    esercizio finito e scritto meglio

--- main.js
+++ main.js
@@ -1,60 +1,85 @@
 $(document).ready(function (){
-  slideImg();
+  allClick();
 });
 
-// creo funzione dove inserire codice
-function slideImg() {
 
-  // scorrimento dx
+
+// functions
+function allClick() {
   $(".next > i").click(function (){
-    var image = $(".images img.active");
-    var circle = $(".nav i.active");
-
-    var imgFirst = $("img.first");
-    var iFirst = $("i.first");
-
-    // se immagine ha classe last è all'ultima foto
-    if (image.hasClass("last")) {
-      image.removeClass("active");
-      circle.removeClass("active");
-
-      imgFirst.addClass("active");
-      iFirst.addClass("active");
-    } else {
-      // cambio immagini tra di loro
-      image.removeClass("active");
-      circle.removeClass("active");
-
-      image.next().addClass("active");
-      circle.next().addClass("active");
-    }
+    nextDx();
   });
 
 
- // scorrimento sx
   $(".prev > i").click(function (){
-    var image = $(".images img.active");
-    var circle = $(".nav i.active");
-
-    var imgLast = $("img.last");
-    var iLast = $("i.last");
-
-    // se immagine ha classe first è alla prima foto
-    if (image.hasClass("first")) {
-
-      image.removeClass("active");
-      circle.removeClass("active");
-
-      imgLast.addClass("active");
-      iLast.addClass("active");
-    } else {
-      // cambio immagini tra di loro
-      image.removeClass("active");
-      circle.removeClass("active");
-
-      image.prev().addClass("active");
-      circle.prev().addClass("active");
-    }
+    prevSx();
   });
 
+
+  $(document).keyup(function(e) {
+    if(e.keyCode == 39){
+      nextDx();
+    }
+
+    if(e.keyCode == 37){
+      prevSx();
+    }
+  });
+}
+
+
+
+
+
+
+
+function nextDx() {
+  // scorrimento dx
+  var image = $(".images img.active");
+  var circle = $(".nav i.active");
+
+  var imgFirst = $("img.first");
+  var iFirst = $("i.first");
+
+  // se immagine ha classe last è all'ultima foto
+  if (image.hasClass("last")) {
+    image.removeClass("active");
+    circle.removeClass("active");
+
+    imgFirst.addClass("active");
+    iFirst.addClass("active");
+  } else {
+    // cambio immagini tra di loro
+    image.removeClass("active");
+    circle.removeClass("active");
+
+    image.next().addClass("active");
+    circle.next().addClass("active");
+  }
+}
+
+function prevSx() {
+  // scorrimento sx
+  var image = $(".images img.active");
+  var circle = $(".nav i.active");
+
+  var imgLast = $("img.last");
+  var iLast = $("i.last");
+
+  // se immagine ha classe first è alla prima foto
+  if (image.hasClass("first")) {
+
+    image.removeClass("active");
+    circle.removeClass("active");
+
+    imgLast.addClass("active");
+    iLast.addClass("active");
+  } else {
+      // cambio immagini tra di loro
+    image.removeClass("active");
+    circle.removeClass("active");
+
+    image.prev().addClass("active");
+    circle.prev().addClass("active");
+  }
 }
